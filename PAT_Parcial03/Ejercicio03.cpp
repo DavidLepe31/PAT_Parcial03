01e6b4c301aa57cbaf925c1eab4e261003f25a33
lobo lepe <d.alconerolepe@gmail.com>
2023-11-25 08:55:06 -0600

    test25854

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -11,15 +11,15 @@ void TimeMap::set(string key, string value, int timestamp)
 {
 	TimeMap* newSet = new TimeMap();
 	int timeprev = 0;
-	if (regis.size() > 0)
+	/*if (regis.size() > 0)
 		timeprev = regis[regis.size() - 1]->time;
 	if ((key.length() > 0 && key.length() < 101) && (value.length() > 0 && value.length() < 101) && (timestamp >0 && timestamp <= pow(10, 7) && timestamp > timeprev))
-	{
+	{*/
 		newSet->key = key;
 		newSet->value = value;
 		newSet->time = timestamp;
 		regis.push_back(newSet);
-	}
+	//}
 }
 
 string TimeMap::get(string key, int timestamp)
@@ -27,8 +27,8 @@ string TimeMap::get(string key, int timestamp)
 	std::vector<TimeMap*>timep;
 	int i = 0;
 	std::string resul="";
-	if ((key.length() > 0 && key.length() < 101) && (timestamp > 0 && timestamp <= pow(10, 7)))
-	{
+	/*if ((key.length() > 0 && key.length() < 101) && (timestamp > 0 && timestamp <= pow(10, 7)))
+	{*/
 		while (i < regis.size())
 		{
 			if (regis[i]->key == key)
@@ -38,11 +38,22 @@ string TimeMap::get(string key, int timestamp)
 			i++;
 		}
 		int j = 0;
-			while (j < timep.size() && timep[j]->time <= timestamp)
+		while (j < timep.size())
+		{
+			if (timep[j]->time = timestamp)
 			{
-				j++;
+				resul = timep[j]->value;
+				break;
 			}
-		if (timep.size() > 0)
+			if (timep[j]->time <= timestamp)
+				if (timep[j]->time > i)
+				{
+					resul = timep[j]->value;
+					i = timep[j]->time;
+				}
+			j++;
+		}
+		/*if (timep.size() > 0)
 		{
 			if (j >= timep.size())
 			{
@@ -60,6 +71,6 @@ string TimeMap::get(string key, int timestamp)
 				resul = timep[j]->value;
 			}
 		}
-	}
+	}*/
 	return resul;
 }
