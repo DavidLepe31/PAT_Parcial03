8389c95d55e97a4c6485e430c3e2adea48427762
lobo lepe <d.alconerolepe@gmail.com>
2023-11-24 19:58:03 -0600

    vamo a ver

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,45 @@
 #include "Ejercicio02.h"
-
+#include<map>
+#include<string>
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
+    Node<char>* tmp = head;
+    Node<char>* count = head;
+    string sequence = "";
+    vector<string>adn;
+    vector<string>tmpadn;
+    std::map<string, int>tmp2;
+    while (count)
+    {
+        if (sequence.size() < 10 && tmp)
+        {
+            sequence += tmp->value;
+            tmp = tmp->next;
+        }
+        else if (sequence.size() >= 10)
+        {
+            tmpadn.push_back(sequence);
+            sequence.erase(sequence.begin());
+            count = count->next;
+        }
+        else {
+            break;
+        }
+
+    }
+    int i = 0;
+    while (i < tmpadn.size())
+    {
+        tmp2[tmpadn[i]]++;
+        i++;
+    }
+    for (auto it : tmp2)
+    {
+        if (it.second > 1)
+        {
+            adn.push_back(it.first);
+        }
+
+    }
     return nullptr;
 }
