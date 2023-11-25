efbadf66ee209a6f6509ebcc41c92569220c3f63
lobo lepe <d.alconerolepe@gmail.com>
2023-11-24 21:29:28 -0600

    prueba 3

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -2,13 +2,45 @@
 
 TimeMap::TimeMap()
 {
+	key = "";
+	value = "";
+	time = 0;
 }
 
 void TimeMap::set(string key, string value, int timestamp)
 {
+	TimeMap* newSet = new TimeMap();
+	newSet->key = key;
+	newSet->value = value;
+	newSet->time = timestamp;
+	regis.push_back(newSet);
 }
 
 string TimeMap::get(string key, int timestamp)
 {
+	std::vector<TimeMap*>timep;
+	int i = 0;
+	while (i < regis.size())
+	{
+		if (key == regis[i]->key)
+		{
+			timep.push_back(regis[i]);
+		}
+		i++;
+	}
+	i = timestamp;
+	while(i>=0)
+	{
+		int j = 0;
+		while (j < timep.size())
+		{
+			if (timep[j]->time==i)
+			{
+				return timep[j]->value;
+			}
+			j++;
+		}
+		i--;
+	}
 	return "";
 }
--- PAT_Parcial03/Ejercicio03.h
+++ PAT_Parcial03/Ejercicio03.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include <string>
+#include<vector>
 
 using std::string;
 
 class TimeMap
 {
 public:
+	string value;
+	string key;
+	int time;
+	
+	std::vector<TimeMap*>regis;
 	TimeMap();
 
 	void set(string key, string value, int timestamp);
